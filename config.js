e0534d2eef37f99b9f1d4b8e536b2b051c2a2e49
Atharv-prog <72858850+Atharv-prog@users.noreply.github.com>
2021-05-28 16:12:43 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyD8uH5HNxNGNqMId6u_QX0LPzwSMkTNJ4g",
+    authDomain: "barter-system-7cf87.firebaseapp.com",
+    projectId: "barter-system-7cf87",
+    storageBucket: "barter-system-7cf87.appspot.com",
+    messagingSenderId: "685883644696",
+    appId: "1:685883644696:web:03b70d979c79563caa1c6d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
